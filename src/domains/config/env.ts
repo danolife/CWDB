87914bac2120f1435d70c912244b763379eb7783
Dany Badr <dany@upflow.io>
2026-01-01 15:56:08 +0100

    explain env setup

--- src/domains/config/env.ts
+++ src/domains/config/env.ts
@@ -1,7 +1,11 @@
 import dotenv from 'dotenv';
 import { z } from 'zod';
 
-dotenv.config({ path: '.env.local' });
+// This shouldn't be necessary because tanstack-start is supposed to load env files
+// File order comes from here: https://tanstack.com/start/latest/docs/framework/react/guide/environment-variables#file-hierarchy-loaded-in-order
+dotenv.config({
+  path: ['.env.local', '.env.production', '.env.development', '.env'],
+});
 
 const serverEnvSchema = z.object({
   DATABASE_URL: z.string(),
